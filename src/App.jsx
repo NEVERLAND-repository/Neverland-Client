e0becc8b3935a4d81658b7e3d54b6ad52fdf893a
Prosper Ikechukwu <mrhendrix62@gmail.com>
2022-04-07 15:24:55 +0100

    Completed Login Page and Responsive for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import SignUp from './pages/authPages/SignUp';
-// import LogIn from './pages/authPages/LogIn';
+import LogIn from './pages/authPages/LogIn';
 
 function App() {
   return (
     <div className="App">
-      {/* <LogIn /> */}
+      <LogIn />
       <SignUp />
     </div>
   );
