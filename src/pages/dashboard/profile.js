ef4e79fad8a7ef92448905139938c46a12b3d5b7
Ekene Nwobodo <nwobodoe71@gmail.com>
2022-04-20 16:39:10 +0100

    Add profile page
    Signed-off-by: Ekene Nwobodo <nwobodoe71@gmail.com>

--- src/pages/dashboard/profile.js
+++ src/pages/dashboard/profile.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import logo from '../../assets/dashboard/mobile-logo.svg';
+import React, { useState } from 'react';
+import logo from '../../assets/images/neverLandLogo-orange.png';
 import Hamburger from '../../assets/icons/burger.svg';
 import profilePatterns from '../../assets/dashboard/patterns.svg';
 import profileIcon from '../../assets/dashboard/profile-icon-dummy.svg';
 import styles from './dashboard.module.css';
+import PrimaryButton from '../../components/buttonComponent/PrimaryButton';
+import Footer from '../homePage/components/footerSection/Footer';
 
 const Profile = () => {
+  const [person, setPerson] = useState({
+    username: '',
+    FullName: '',
+    gender: 'other',
+  });
+
+  const [people, setPeople] = useState([]);
+
+  const handleChange = (e) => {
+    const { name } = e.target;
+    const { value } = e.target;
+    setPerson({ ...person, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (person.firstName || person.email || person.age) {
+      const newPerson = { ...person };
+      setPeople([...people, newPerson]);
+    }
+  };
   return (
     <>
       <header className={ `${ styles.profile__Nav } ${ styles.header }` }>
@@ -34,23 +57,93 @@ const Profile = () => {
           </div>
         </div>
       </section>
-      <form>
-        <div>
-          <div>
-            <h1 className='title'>Full name</h1>
-            <p className='subtitle'>Customise your accounts</p>
+      <section className={ styles.form__section }>
+        <form className={ styles.form }>
+          <div className={ styles.form__group }>
+            <div className={ styles.form__group__content__side }>
+              <h1 className={ styles.title }>Full name</h1>
+              <p className={ styles.subtitle }>Customise your accounts</p>
+            </div>
+            <div className={ styles.form_group_input__side }>
+              <input
+                type='text'
+                name='FullName'
+                id='FullName'
+                value={ person.FullName }
+                className={ styles.form__group__input }
+                onChange={ handleChange }
+              />
+            </div>
           </div>
-          <div className='input'>
-            <input
-              type='text'
-              name='username'
-              id='username'
-              value={ username }
-              className={ styles.form__control }
-            />
+          <div className={ styles.form__group }>
+            <div className={ styles.form__group__content__side }>
+              <h1 className={ styles.title }>Username</h1>
+              <p className={ styles.subtitle }>Personalise your library</p>
+            </div>
+            <div className={ styles.form_group_input__side }>
+              <input
+                type='text'
+                name='username'
+                id='username'
+                value={ person.username }
+                className={ styles.form__group__input }
+                onChange={ handleChange }
+              />
+            </div>
           </div>
-        </div>
-      </form>
+          <div className={ styles.form__group }>
+            <div className={ styles.form__group__content__side }>
+              <h1 className={ styles.title }>Gender</h1>
+              <p className={ styles.subtitle }>How you like to be identified</p>
+            </div>
+            <div className={ `${ styles.form_group_input__side } ${ styles.radio }` }>
+              <div className={ styles.radio__control }>
+                <input
+                  id='male'
+                  type='radio'
+                  value='male'
+                  name='gender'
+                  checked={ person.gender === 'male' }
+                  onChange={ handleChange }
+                />
+                <label htmlFor='male'>
+                  Male
+                </label>
+              </div>
+              <div className={ styles.radio__control }>
+                <input
+                  id='female'
+                  type='radio'
+                  value='female'
+                  name='gender'
+                  checked={ person.gender === 'female' }
+                  onChange={ handleChange }
+                />
+                <label htmlFor='female'>
+                  Female
+                </label>
+              </div>
+              <div className={ styles.radio__control }>
+                <input
+                  id='other'
+                  type='radio'
+                  value='other'
+                  name='gender'
+                  checked={ person.gender === 'other' }
+                  onChange={ handleChange }
+                />
+                <label htmlFor='other'>
+                  Other
+                </label>
+              </div>
+            </div>
+          </div>
+          <div className={ styles.save__changes__btn }>
+            <PrimaryButton label='Save Changes' />
+          </div>
+        </form>
+      </section>
+      <Footer />
     </>
   );
 };
