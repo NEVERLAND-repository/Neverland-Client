a843f76008e8c09cd8338d9fd402719325106009
cyrilchukwuebuka <muofunanya3@gmail.com>
2022-04-29 17:17:20 +0100

    protected the dashboard route

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { getUserData } from '../../store/slice/neverlandUserSlice';
 // import Profile from './profile'
 import Sidebar from './sidebar'
 
 const Dashboard = () => {
+  const token = useSelector(getUserData)?.token;
+  const navigate = useNavigate()
+
+  if (!token) {
+    navigate('/home')
+  }
   return (
     <Sidebar />
   )
